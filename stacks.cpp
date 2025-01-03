943ac5788805fe71874c997cc02ebe730ad3f3a6
amiralitariq <74525936+amiralitariq@users.noreply.github.com>
2025-01-03 19:45:04 +0500

    Update stacks.cpp

--- stacks.cpp
+++ stacks.cpp
@@ -1,1 +1,25 @@
 
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    stack<string> cars;
+
+    /* Adding Elements in Stack */
+    cars.push("Tesla");
+    cars.push("BMW");
+    cars.push("Audi");
+    cars.push("Hyundai");
+
+    /* As Stack follows Last In First Out (LIFO), we print elements using top() and pop() */
+    cout << "Cars in the stack (LIFO order):" << endl;
+
+    while (!cars.empty()) {
+        cout << cars.top() << endl; // Access the top element
+        cars.pop();                 // Remove the top element
+    }
+
+    return 0;
+}
